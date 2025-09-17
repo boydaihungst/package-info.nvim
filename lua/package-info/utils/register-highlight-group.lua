b0211305d24ebc27cbc9ef8a3117513a44afcb63
Huy Hoang <boydaihungst@gmail.com>
2025-09-18 04:00:12 +0700

    feat!: rework hightlight group to accept more than just text color. Replace `colors` option with `highlights`
    * feat!: rework highlight groups to accept more than just text color.
      - Check [:help nvim_set_hl()](https://neovim.io/doc/user/api.html#nvim_set_hl()) for supported attributes.
      - Replace `colors` with `hightlights` option. Fully backward-compatible with old `colors` but a warn notification will show up.
      - Docs: updated with new highlights option.
      - Compatible with nvim 0.6.0
    
    * fix:
      - Lazy loading highlight group.
      - Highlight group merge priority: user setup options > color scheme > default
    
    * test:
      - Updated with new highlights options.

--- lua/package-info/utils/register-highlight-group.lua
+++ lua/package-info/utils/register-highlight-group.lua
@@ -1,13 +1,39 @@
---- Register given highlight group
--- @param group: string - highlight group to register
--- @param color: string - color to use with the highlight group
-return function(highlight_group, color)
-    local type = "guifg"
+local constants = require("package-info.utils.constants")
+local logger = require("package-info.utils.logger")
 
-    --- 256 color support
-    if not vim.o.termguicolors then
-        type = "ctermfg"
+local M = {}
+
+function M:get_colorscheme_hl()
+    local colorscheme_highlights = {}
+    for hl_opts_name, hl_group_name in pairs(constants.HIGHLIGHT_GROUPS) do
+        local color_scheme_hl_exist, color_scheme_hl = pcall(self.get_hl, hl_group_name)
+        if color_scheme_hl_exist and not vim.tbl_isempty(color_scheme_hl) then
+            colorscheme_highlights[hl_opts_name] = color_scheme_hl
+        end
     end
-
-    vim.cmd("highlight " .. highlight_group .. " " .. type .. "=" .. color)
+    return colorscheme_highlights
 end
+-- Compatibility wrapper for getting highlights
+function M.get_hl(name)
+    if vim.api.nvim_get_hl then
+        -- Neovim 0.9+
+        return vim.api.nvim_get_hl(0, { name = name })
+    else
+        -- Neovim 0.6–0.8
+        local ok, hl = pcall(vim.api.nvim_get_hl_by_name, name, true)
+        if not ok then
+            return {}
+        end
+        return hl or {}
+    end
+end
+
+function M.set_hl(group, opts)
+    local success, result = pcall(vim.api.nvim_set_hl, 0, group, opts)
+    if not success then
+        logger.error("Error setting highlight: " .. result)
+    end
+    return
+end
+
+return M
